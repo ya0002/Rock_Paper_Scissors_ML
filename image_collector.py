8739476af5cbdd6cd1bfb9552490ae9be6b42b2b
Mohammad yusuf Jamal Aziz Azmi <yusufjamal2773@gmail.com>
2020-05-28 08:34:53 +0530

    completed image collector

--- image_collector.py
+++ image_collector.py
@@ -3,7 +3,10 @@ import keyboard
 import os
 
 def save_image(type_file, sample, n_samples_collected, frame,n_images):
-    os.chdir(current_dir + '\\' + file_name[type_file])
+    if n_images>(0.7*500):
+        os.chdir(current_dir + '\\' +'Dataset'+'\\'+dataset_type[1]+'\\'+ file_name[type_file])
+    else:
+        os.chdir(current_dir + '\\' +'Dataset'+'\\'+dataset_type[0]+'\\'+ file_name[type_file])
     cv2.imwrite(f'{file_name[type_file]}{n_samples_collected}.png', frame[105:395, 285:595])
     n_samples_collected += 1
     n_images +=1
@@ -14,16 +17,23 @@ def save_image(type_file, sample, n_samples_collected, frame,n_images):
 
 
 # creating files to store the images in
-file_name=['rock','paper','scissor']
+
 current_dir=os.getcwd()
+
+dataset_type=['train','test']
+file_name=['rock','paper','scissor','dummy']
 try:
-    for i in file_name:
-        os.mkdir(current_dir+'\\'+i)
+    os.mkdir(current_dir + '\\' + 'Dataset')
+    for name in dataset_type:
+        os.mkdir(current_dir+'\\'+'Dataset'+'\\'+name)
+        for i in file_name:
+            os.mkdir(current_dir + '\\' +'Dataset'+'\\'+name+'\\'+ i)
 except:
     pass
 
-Rock,Paper,Scissor=False,False,False
-n_Rock,n_Paper,n_Scissor,n_images=0,0,0,0
+
+Rock,Paper,Scissor,Dummy=False,False,False,False
+n_Rock,n_Paper,n_Scissor,n_Dummy,n_images=0,0,0,0,0
 
 vid=cv2.VideoCapture(0)
 while True:
@@ -36,11 +46,12 @@ while True:
     cv2.putText(frame, 'Rock: '+str(n_Rock), (10, 26), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
     cv2.putText(frame, 'Paper: '+str(n_Paper), (10, 60), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
     cv2.putText(frame, 'Scissors: '+str(n_Scissor), (10, 90), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
+    cv2.putText(frame, 'Dummy: '+str(n_Dummy), (10, 120), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
     cv2.rectangle(frame,(280,100),(600,400),(0,0,255),2)
     cv2.imshow('video', frame)
 
     if keyboard.is_pressed('r'):
-        if (Paper,Scissor)==(False,False):
+        if (Paper,Scissor,Dummy)==(False,False,False):
             Rock=True
             print('collecting rock')
         else:
@@ -51,7 +62,7 @@ while True:
 
 
     if keyboard.is_pressed('p'):
-        if (Rock,Scissor)==(False,False):
+        if (Rock,Scissor,Dummy)==(False,False,False):
             Paper=True
             print('collecting paper')
         else:
@@ -60,8 +71,9 @@ while True:
     if Paper:
         Paper,n_Paper,n_images=save_image(1,Paper,n_Paper,frame,n_images)
 
+
     if keyboard.is_pressed('s'):
-        if (Rock,Paper)==(False,False):
+        if (Rock,Paper,Dummy)==(False,False,False):
             Scissor=True
             print('collecting scissor')
         else:
@@ -69,6 +81,17 @@ while True:
 
     if Scissor:
         Scissor,n_Scissor,n_images=save_image(2,Scissor,n_Scissor,frame,n_images)
+
+
+    if keyboard.is_pressed('d'):
+        if (Rock,Paper, Scissor) == (False,False, False):
+            Dummy = True
+            print('collecting dummy')
+        else:
+            print('unavailable right now')
+
+    if Dummy:
+        Dummy, n_Dummy, n_images = save_image(3, Dummy, n_Dummy, frame, n_images)
 
     if cv2.waitKey(1) & 0xFF==ord('q'):
         break

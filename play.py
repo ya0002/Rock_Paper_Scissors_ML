09cf0def33a4971e0d750e04f04076a9e742662e
Mohammad yusuf Jamal Aziz Azmi <yusufjamal2773@gmail.com>
2020-05-30 01:25:43 +0530

    game completed

--- play.py
+++ play.py
@@ -3,10 +3,17 @@ import tensorflow as tf
 import cv2
 import numpy as np
 import random
+import os
+
+current=os.getcwd()
 
 model=tf.keras.models.load_model('rock_paper_scissor.h5')
 
 move={0:'nothing',1:'paper',2:'rock',3:'scissor'}
+move_cartoon=[cv2.imread(current+'\\'+'Props'+'\\'+'dummy.PNG'),cv2.imread(current+'\\'+'Props'+'\\'+'paper.PNG'),cv2.imread(current+'\\'+'Props'+'\\'+'rock.PNG'),cv2.imread(current+'\\'+'Props'+'\\'+'scissor.PNG')]
+
+#initialize it to dummy.PNG
+img_cartoon=move_cartoon[0]
 
 n_frame,index,c_index=[0]*3
 points=[0,0]  # index=0 for computer   and index=1 for user(YOU)
@@ -18,17 +25,29 @@ while True:
         break
 
     frame=cv2.flip(frame,1)
-    # cv2.putText(frame, 'Rock: '+str(n_Rock), (10, 26), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
-    cv2.putText(frame, 'Your Move: ' + move[index], (450, 26), cv2.FONT_HERSHEY_SIMPLEX, .5, (255, 255, 255), 1)
-    cv2.putText(frame, 'Computer: ' + move[c_index], (10, 26), cv2.FONT_HERSHEY_SIMPLEX, .5, (255, 255, 255), 1)
-    cv2.putText(frame, f'{points[0]} - {points[1]}', (280, 26), cv2.FONT_HERSHEY_SIMPLEX, .5, (255, 255, 255), 2)
+
+    frame[0:480,0:278]=img_cartoon
+    frame[404:480,278:640]=np.full((76,362,3),255)
+    frame[0:404,603:640]=np.full((404,37,3),255)
+    frame[0:101,278:603]=np.full((101,325,3),255)
+
+    # if you wanna keep the transparency  P.S=Black won't show     I didn't do it because my hardware wasn't upto it, it was lagging a lot
+    # for i in range(200):
+    #     for j in range(200):
+    #         x=img_cartoon[i,j]
+    #         if list(x)!=[0,0,0]:
+    #             frame[i, j] = x
+
+    cv2.putText(frame, 'Your Move: ' + move[index], (450, 26), cv2.FONT_HERSHEY_SIMPLEX, .5, (0, 0, 0), 1)
+    cv2.putText(frame, 'Computer: ' + move[c_index], (10, 26), cv2.FONT_HERSHEY_SIMPLEX, .5, (0, 0, 0), 1)
+    cv2.putText(frame, f'{points[0]} - {points[1]}', (280, 26), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 2)
     cv2.rectangle(frame,(280,100),(600,400),(0,0,255),2)
     cv2.imshow('video', frame)
 
     if keyboard.is_pressed('r'):
         points=[0,0]
 
-    if n_frame % 40 == 0:
+    if n_frame % 50 == 0:
         img_predict=frame[105:395, 285:595]
         img_predict = np.reshape(img_predict, (1, 290, 310, 3))
         prediction=model.predict(img_predict)
@@ -37,6 +56,7 @@ while True:
         print(index, move[index])
         if index!=0:
             c_index=random.randint(1,3)
+            img_cartoon = move_cartoon[c_index]
             if index!=c_index:
                 if (index,c_index)!=(1,3) and (index,c_index)!=(3,1):
                     if index<c_index:
@@ -50,6 +70,7 @@ while True:
                         points[0]+=1
         else:
             c_index=0
+            img_cartoon=move_cartoon[0]
 
     n_frame+=1
 
